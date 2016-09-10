6d623e8097a9050a30d097eb281a1aece8e9a0ff
Keith Mendoza <pantherse@gmail.com>
2016-09-10 15:52:12 -0700

    Initial app and logger configuration

--- src/under_durest.cpp
+++ src/under_durest.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 #include <string>
 #include <stdexcept>
+#include <fstream>
 
 #include <unordered_map>
 
-//#include <unistd.h>
+#include <unistd.h>
 #include <getopt.h>
+
+#include <jsoncpp/json/json.h>
 
 #include <log4cpp/Category.hh>
 #include <log4cpp/PropertyConfigurator.hh>
@@ -13,6 +16,8 @@
 #include <log4cpp/NDC.hh>
 
 using namespace std;
+using namespace Json;
+using namespace log4cpp;
 
 typedef unordered_map<string, string> CmdLines;
 
@@ -33,15 +38,16 @@ CmdLines parseArgs(int argc, char **argv)
     int opt;
     while((opt = getopt_long(argc, argv, "c:t:", opts, nullptr)) != -1)
     {
-        cout << "Value of opt: \"" << opt << "\"" << endl;
+        // DO NOT DELETE COMMENTED OUT CODE
+        //cout << "Value of opt: \"" << opt << "\"" << endl;
         switch(opt)
         {
         case 'c':
-            cout << "c option found. Arg: " << optarg << endl;
+            //cout << "c option found. Arg: " << optarg << endl;
             cmds["config"] = optarg;
             break;
         case 't':
-            cout << "t option found. Arg: " << optarg << endl;
+            //cout << "t option found. Arg: " << optarg << endl;
             cmds["test"] = optarg;
             break;
         default:
@@ -53,11 +59,33 @@ CmdLines parseArgs(int argc, char **argv)
     return move(cmds);
 }
 
+Value parseConfig(const string& fileName)
+{
+    ifstream inFile(fileName.c_str(), ios::in | ios::binary);
+    if(!inFile.is_open())
+    {
+        cerr << "File not found: " << fileName << "\n";
+        throw runtime_error("Config file " + fileName + " not found");
+    }
+
+    Value root;
+    inFile >> root;
+
+    /* DO NOT DELETE
+    cout << "Config members:\n";
+    for(auto i : root.getMemberNames())
+        cout << "\t" << i << endl;
+    */
+
+    return move(root);
+}
+
+bool logReady = false;
+
 int main(int argc, char **argv)
 {
-    auto cmds = parseArgs(argc, argv);
-
     // Check all options are there
+    auto cmds = parseArgs(argc, argv);
     {
         string expectedOptions[] = {
             "config",
@@ -75,6 +103,31 @@ int main(int argc, char **argv)
                 usage(argv[0]);
                 exit(EXIT_FAILURE);
             }
+    }
+
+    try
+    {
+        //Load config file
+        auto config = parseConfig(cmds["config"]);
+
+        //Configure logger
+        PropertyConfigurator::configure(config["logconfig"].asString());
+
+        Category &root = Category::getRoot();
+        logReady = true;
+        root.infoStream() << "under_durest initialized";
+    }
+    catch(const ConfigureFailure &e)
+    {
+        cerr << "Error encountered configuring logger. Cause: " << e.what() << endl;
+        return EXIT_FAILURE;
+    }
+    catch(const exception &e)
+    {
+        if(logReady)
+            Category::getRoot().errorStream() << "Error encountered. Cause: " << e.what();
+        else
+            cerr << "Error encountered. Cause: " << e.what() << endl;
     }
 
     return 0;
